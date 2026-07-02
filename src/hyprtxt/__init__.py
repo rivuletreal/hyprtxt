2739fc841914e9781b7cdb41a38c4e0f18f520e8
Noah Cameron <NoahBackwards@proton.me>
2026-07-02 15:45:24 -0700

    add a bit more url sanitization

--- src/hyprtxt/__init__.py
+++ src/hyprtxt/__init__.py
@@ -4,7 +4,7 @@ from .core import Element, Page
 def add_page(route: str, page: type[Page]) -> None:
     from . import core
 
-    core.pages["/" + route.removeprefix("/")] = page
+    core.pages["/" + route.removeprefix("/").removesuffix("/")] = page
 
 
 __all__ = ["Element", "Page", "add_page"]
